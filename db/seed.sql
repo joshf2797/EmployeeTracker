911055a6771094240cdb9630c959e43a61890bb3
Joshua <joshf2797@gmail.com>
2023-02-06 22:09:52 -0700

    initial push, created schema and seed files

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (department_name)
+VALUES ("Coach"),
+       ("Player"),
+       ("Trainer"),
+       ("Executive");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("")
